cc1c3fa1d8e5f1e49466f73d2fc8143f3ed94737
Peter Rupprecht <PTRRupprecht@users.noreply.github.com>
2026-02-16 15:23:38 +0100

    Less strict requirements for packages
    Remove Spyder and specific Torch version from requirement instructions

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="cascadeTorch",
+    name="CascadeTorch",
     version="2.0",
-    description="Calibrated inference of spiking from calcium ΔF/F data using deep networks",
+    description="Calibrated inference of spiking from calcium ΔF/F data using deep networks with PyTorch",
     author="Peter Rupprecht",
     author_email="",
     packages=find_packages(),
@@ -12,10 +12,9 @@ setup(
         "numpy",
         "scipy",
         "matplotlib",
-        "torch==2.5.1",  # pip install CPU and GPU tensorflow
+        "torch>=2.5",  # pip install CPU and GPU tensorflow
         "h5py",
         "seaborn",
         "ruamel.yaml",
-        "spyder",
     ],
 )
